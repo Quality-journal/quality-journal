b9e80cd9276522d52ee0cb1bccc4ab00ada9bcfd
unknown <amarkovic@projectland.rs>
2021-02-05 10:57:15 +0100

    about

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -10,7 +10,7 @@
                 <div class="block lg:hidden">
                     <button @click='toggleMobileMenu' class="flex items-center px-3 py-2 text-teal-200 hover:text-white">
                         <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-                    </button>
+                    </button> 
                 </div>
                 <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto" v-bind:class="{ 'hidden': !showMobileMenu}">
                     <div class="text-lg lg:flex-grow">
